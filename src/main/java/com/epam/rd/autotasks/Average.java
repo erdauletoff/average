3d3d4de500ea1872d14d01208e1a9f39d6173dcf
Darkhan <darkhan_erdawletov@student.itpu.uz>
2023-10-18 14:49:05 +0500

    done!

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -3,10 +3,26 @@ package com.epam.rd.autotasks;
 import java.util.Scanner;
 
 public class Average {
+    public static int sum(int[] arr) {
+        int res = 0;
+
+        for (int n: arr) {
+            res += n;
+        }
+
+        return res;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        String[] in_data = scanner.nextLine().trim().split(" ");
+        int len = in_data.length;
+        int[] nums = new int[len - 1];
 
+        for (int i = 0; i < len - 1; i++) {
+            nums[i] = Integer.parseInt(in_data[i]);
+        }
+
+        System.out.println(sum(nums) / (len - 1));
     }
 
 }
